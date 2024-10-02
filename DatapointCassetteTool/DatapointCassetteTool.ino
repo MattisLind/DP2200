c148bc1d3a9b2446c655ec20a315f8c038661db5
Mattis Lind <mattis@mattisborgen.se>
2024-10-02 21:24:34 +0200

    Adding DatapointCassetteTool.

---
+++ DatapointCassetteTool/DatapointCassetteTool.ino
@@ -0,0 +1,144 @@
+#include <SPI.h>
+#include "RingBuffer.h"
+
+class RingBuffer txBuffer;
+
+/*
+ * 
+ *t
+
+
+
+Lastbit = 0
+Bitsinoutbuffer = 0
+
+fill ringbuffer with  0 pattern (even number of bytes worth. Approximately 270 bytes
+
+
+While not out of buffer
+  // add the sync pattern
+  call function too shift in bit above with argument 0
+  call function too shift in bit above with argument 1
+  call function too shift in bit above with argument 0
+  Get one byte from buffer
+  loop for all 8 bits
+    Start with MSB bit. Mask for this bit 
+    call function too shift in bit above with argument the bit from above
+  Now we have 22 bits in the buffer
+  Calculate number of shifts: 32-22-bitsinoutbuffer = 10 shifts now
+  shift ten times and or it to the outbitbuffer.
+  we now have 22 bits shifted to the left.
+  loop until bitsinoutbuffer is less than 8. I.e less than a full byte
+    extract the top byte and put into the output ring buffer.
+    shift 8 steps
+  After first round we should have the value 6 in bitsinoutbuffer. I.e. next turn in the outher loop we will shift 4 times left.
+When buffer is out of chars
+  shift in 0, 1 0 sync char
+  shift in ff byte
+  fill up the out buffer to full 32 bits
+  transfer those to the ringbuffer
+
+Then fill ringbuffer with  0 pattern (even number of bytes worth. Approximately 270 bytes
+
+ */
+
+SPIClass SPI_2(2);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  SPI_2.beginSlave(); //Initialize the SPI_2 port.
+  SPI_2.setBitOrder(MSBFIRST); // Set the SPI_2 bit order
+  SPI_2.setDataMode(SPI_MODE0); //Set the  SPI_2 data mode 0
+  Serial.begin(9600);
+  Serial.print("DATAPOINT CASSETTE TOOL> ");
+
+}
+
+int flag=0;
+
+extern "C" void __irq_spi2 (void) {
+  unsigned char ch;     
+  if (spi_is_tx_empty(SPI2)){  
+    if (txBuffer.isBufferEmpty()) {
+      spi_tx_reg(SPI2, 0xff);  
+      if (flag) {
+        spi_irq_disable(SPI2, SPI_RXNE_INTERRUPT);  
+        flag = 0;
+      }
+    } else {
+      flag = 1;
+      ch = txBuffer.readBuffer();         
+      spi_tx_reg(SPI2, ch);  
+    }    
+  }
+  if (spi_is_rx_nonempty(SPI2)) {
+    ch = spi_rx_reg(SPI2);
+  }
+}
+
+/*
+ * 
+ *  Function shift in a bit in bitaccumulator variable:
+ Test bit for 0 or 1.
+  If 1
+    invert lastbit
+    shift in lastbit in bitaccumulaor variable from the right
+    Do this again.
+  If 0
+    invert lastbit
+    shift in lastbit in bitaccumulaor variable from the right
+    invert lastbit
+    shift in lastbit in bitaccumulaor variable from the righ
+ */
+
+char lastBit = 0;
+long bitAcc = 0;
+
+void shiftIn (char bit) {
+  if (bit) {
+    lastBit ^= 1; 
+    bitAcc = bitAcc << 1;
+    bitAcc = bitAcc | lastBit;
+    bitAcc = bitAcc << 1;
+    bitAcc = bitAcc | lastBit;       
+  } else {
+    lastBit ^= 1; 
+    bitAcc = bitAcc << 1;
+    bitAcc = bitAcc | lastBit;
+    lastBit ^= 1; 
+    bitAcc = bitAcc << 1;
+    bitAcc = bitAcc | lastBit;     
+  }
+}
+
+void writeCassette() {
+  spi_tx_reg(SPI2, 0xff); // dummy write 
+  spi_rx_reg(SPI2); // dummy read
+  spi_irq_enable(SPI2, SPI_RXNE_INTERRUPT);
+  // fill ringbuffer with  "1" pattern (even number of bytes worth. Approximately 270 bytes
+  for (int i=0; i++; i <270) { 
+    while (txBuffer.isBufferFull()) Serial.write('.');
+    txBuffer.writeBuffer(0xcc);
+  }
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  int ch;
+  if (Serial.available()) {
+    ch = Serial.read();
+    switch (c) {
+      case 'S':  // Start 
+      case 's':
+        break;
+      case 'T':  // Stop
+      case 't':
+        break;
+      case 'W':  // Write
+      case 'w':
+        writeCassette();
+        break;
+    }
+  }
+}
---
+++ DatapointCassetteTool/RingBuffer.h
@@ -0,0 +1,20 @@
+#ifndef _RING_BUFFER_
+#define _RING_BUFFER_
+#define SIZE 32
+
+class RingBuffer {
+  volatile int inPtr, outPtr, bufferFull, bufferEmpty;
+  unsigned char buffer[SIZE];
+public:
+  int isBufferFull () {
+    return bufferFull;
+  };
+  int isBufferEmpty () {
+    return bufferEmpty;
+  }
+  void writeBuffer (unsigned char data);
+  unsigned char readBuffer ();
+  void  initBuffer ();
+};
+
+#endif
